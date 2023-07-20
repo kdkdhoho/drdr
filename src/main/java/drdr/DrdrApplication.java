621bb180546c533af8f4ecc44c6b55042eec2548
kdkdhoho <hkim4410@naver.com>
2023-07-20 22:27:16 +0900

    chore: 개발 환경 세팅 (#3)

---
+++ src/main/java/drdr/DrdrApplication.java
@@ -0,0 +1,13 @@
+package drdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrdrApplication.class, args);
+	}
+
+}
